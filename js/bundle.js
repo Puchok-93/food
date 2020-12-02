7582c60a6f431e81fa714c0814112e510de0bb0f
Puchok-93 <54364298+Puchok-93@users.noreply.github.com>
2020-12-02 23:51:45 +0300

    Реализация навигации слайдера

--- js/bundle.js
+++ js/bundle.js
@@ -84,6 +84,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => __WEBPACK_DEFAULT_EXPORT__
 /* harmony export */ });
 function slider() {
+    const slider = document.querySelector('.offer__slider');
     const sliderWrapper = document.querySelector('.offer__slider-wrapper');
     const sliderInnerWrapper = sliderWrapper.querySelector('.offer__slider-inner');
     const slides = document.querySelectorAll('.offer__slide');
@@ -94,6 +95,7 @@ function slider() {
     const widthSlide = window.getComputedStyle(sliderWrapper).width;
     let slideIndex = 1;
     let offset = 0;
+
 
     if (slides.length < 10) {
         totalSlide.textContent = `0${slides.length}`;
@@ -127,6 +129,11 @@ function slider() {
             currentSlide.textContent = slideIndex;
         }
 
+        dots.forEach(dot => {
+            dot.classList.remove('dot__active');
+        });
+        dots[slideIndex - 1].classList.add('dot__active');
+
         sliderInnerWrapper.style.transform = `translateX(-${offset}px)`;
     }
 
@@ -149,8 +156,51 @@ function slider() {
             currentSlide.textContent = slideIndex;
         }
 
+        dots.forEach(dot => {
+            dot.classList.remove('dot__active');
+        });
+        dots[slideIndex - 1].classList.add('dot__active');
+
         sliderInnerWrapper.style.transform = `translateX(-${offset}px)`;
     }
+
+    const indicators = document.createElement('ol');
+    const dots = [];
+    indicators.classList.add('carousel-indicators');
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.classList.add('dot');
+        indicators.append(dot);
+
+        if (i == 0) {
+            dot.classList.add('dot__active');
+        }
+
+        dots.push(dot);
+    };
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+            slideIndex = slideTo;
+            offset = +widthSlide.slice(0, widthSlide.length - 2) * (slideTo - 1);
+            sliderInnerWrapper.style.transform = `translateX(-${offset}px)`;
+
+            if (slides.length < 10) {
+                currentSlide.textContent = `0${slideIndex}`;
+            } else {
+                currentSlide.textContent = slideIndex;
+            }
+            
+            dots.forEach(dot => {
+                dot.classList.remove('dot__active');
+            });
+            dots[slideIndex - 1].classList.add('dot__active');
+        });
+    });
 
     nextSlideButton.addEventListener('click', showNextSlide);
     prevSlideButton.addEventListener('click', showPrevSlide);
